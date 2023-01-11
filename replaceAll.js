5e1505de4de2ab06a0650d451ee53c0c8bb1495e
aahedi <aahedi@gmail.com>
2023-01-11 18:55:13 +0700

    Update replaceAll.js

--- replaceAll.js
+++ replaceAll.js
@@ -1,5 +1,4 @@
 //replaceAll library js by Aahedi https://github.com/aahedi/replaceAll
-
 function replaceAll(str, find, replace, options) {
 	var gx = 'g';
 	var regExp = new RegExp('\\b' + find + '\\b','g');
